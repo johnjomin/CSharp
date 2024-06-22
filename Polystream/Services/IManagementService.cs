7a6a205ff6ada75791dfdf4e4dd92ec2ba913892
Jomin John <jominjhn@googlemail.com>
2024-06-22 15:54:28 +0100

    Initial Commit

---
+++ Polystream/Services/IManagementService.cs
@@ -0,0 +1,96 @@
+﻿using PolystreamAssessment.Models;
+using PolystreamAssessment.Models.BodyModels;
+using PolystreamAssessment.Models.Modeller;
+using PolystreamAssessment.Models.Response;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PolystreamAssessment.Services
+{
+    public interface IManagementService
+    {
+        /// <summary>
+        /// Get user by user id.
+        /// </summary>
+        /// <returns></returns>
+        Task<List<User>> GetUserByUserId(string userId);
+
+        /// <summary>
+        /// Get user by user id.
+        /// </summary>
+        /// <returns></returns>
+        Task<List<Machine>> GetMachineByMachineId(string machineId);
+
+        /// <summary>
+        /// Get user by user id.
+        /// </summary>
+        /// <returns></returns>
+        Task<List<Application>> GetApplicationByApplicationId(string applicationId);
+
+        /// <summary>
+        /// Get user by user id.
+        /// </summary>
+        /// <returns></returns>
+        Task<CouchDbCreateDocumentResult> PostServiceUser(UserBody body);
+
+        /// <summary>
+        /// Get user by user id.
+        /// </summary>
+        /// <returns></returns>
+        Task<CouchDbCreateDocumentResult> PostServiceMachine(MachineBody machine);
+
+        /// <summary>
+        /// Get user by user id.
+        /// </summary>
+        /// <returns></returns>
+        Task<CouchDbCreateDocumentResult> PostServiceApplication(ApplicationBody body);
+
+        /// <summary>
+        /// Get user by user id.
+        /// </summary>
+        /// <returns></returns>
+        Task<bool> DeleteServiceUser(string userId);
+
+        /// <summary>
+        /// Get user by user id.
+        /// </summary>
+        /// <returns></returns>
+        Task<bool> DeleteServiceMachine(string machineId);
+
+        /// <summary>
+        /// Get user by user id.
+        /// </summary>
+        /// <returns></returns>
+        Task<bool> DeleteServiceApplication(string applicationId);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        /// <summary>
+        /// Get pages and group.
+        /// </summary>
+        /// <returns></returns>
+        //Task<ServiceManagementResponse> DeleteKeywordfromBricks(string id, string name, string pageId);
+
+    }
+}
